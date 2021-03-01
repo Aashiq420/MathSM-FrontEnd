ef584569164c160b96625e9f753ab099722b47c8
Aashiq <adams.aashiq@gmail.com>
2021-03-01 11:36:48 +0200

    monday 1 march

---
+++ assets/scripts/landing.js
@@ -0,0 +1,21 @@
+function createUser() {
+  const inputs = document.getElementsByTagName("input");
+
+  fetch("http://127.0.0.1:5000/landing/", {
+    method: "POST",
+    body: JSON.stringify({
+      fullname: inputs[0].value,
+      username: inputs[0].value,
+      email: inputs[0].value,
+      password: inputs[0].value,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      alert("New user created successfully");
+      document.getElementById("register").reset();
+    });
+}
