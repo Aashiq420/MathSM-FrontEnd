273b2793d7c3a17ca3b7c51af1a5fb05cf46a60a
Aashiq Adams <71071675+Aashiq420@users.noreply.github.com>
2021-03-12 15:29:08 +0200

    Add files via upload

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -69,7 +69,7 @@ function displayPosts() {
             <p>${post.image}</p>
           </div>
           <div class="button">
-          <button class="btn">View comments</button>
+          <button id="myBtn" class="btn" onclick="openModal()">View comments</button>
           <button class="btn">Post comments</button>
           </div>
           <br><br>
@@ -109,3 +109,30 @@ document.addEventListener(
     passive: true,
   }
 );
+
+//post comment js
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+function openModal() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
